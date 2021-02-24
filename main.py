76e7702fda1a334c39a43d7954e70e1d26c0a0d8
AndruKP <andruhovski@outlook.com>
2021-02-24 11:46:01 +0200

    Defeated easy AI

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
+import random
+
 import sc2
 from sc2 import run_game, maps, Race, Difficulty
 from sc2.player import Bot, Computer
 from sc2.constants import NEXUS, PROBE, PYLON, ASSIMILATOR, GATEWAY, CYBERNETICSCORE, STALKER
-
-
-# all constants imports from other packages - IDE is not so clever to see it, but there is no error
 
 
 class AndruBot(sc2.BotAI):
@@ -16,11 +15,13 @@ class AndruBot(sc2.BotAI):
         await self.expand()
         await self.offensive_force_buildings()
         await self.build_offensive_force()
+        await self.attack()
 
     async def build_workers(self):
         for nexus in self.units(NEXUS).ready:
-            if self.can_afford(PROBE) and nexus.noqueue:
-                await self.do(nexus.train(PROBE))
+            if self.units(PROBE).amount < 70:
+                if self.can_afford(PROBE) and nexus.noqueue:
+                    await self.do(nexus.train(PROBE))
 
     async def build_pylons(self):
         if self.supply_left < 5 and not self.already_pending(PYLON):
@@ -38,28 +39,45 @@ class AndruBot(sc2.BotAI):
                 worker = self.select_build_worker(vespene.position)
                 if worker is None:
                     break
-
                 if not self.units(ASSIMILATOR).closer_than(1.0, vespene).exists:
                     await self.do(worker.build(ASSIMILATOR, vespene))
 
     async def expand(self):
-        if self.units(NEXUS).amount < 3 and self.can_afford(NEXUS):
+        if (self.units(NEXUS).amount < 3 and not self.already_pending(NEXUS)) and self.can_afford(NEXUS):
             await self.expand_now()
 
     async def offensive_force_buildings(self):
         if self.units(PYLON).ready.exists:
             pylon = self.units(PYLON).ready.random
-            if self.units(GATEWAY).ready.exists:
+            if self.units(GATEWAY).ready.exists and not self.units(CYBERNETICSCORE):
                 if self.can_afford(CYBERNETICSCORE) and not self.already_pending(CYBERNETICSCORE):
                     await self.build(CYBERNETICSCORE, near=pylon)
-            else:
+            elif len(self.units(GATEWAY)) < 3:
                 if self.can_afford(GATEWAY) and not self.already_pending(GATEWAY):
                     await self.build(GATEWAY, near=pylon)
 
     async def build_offensive_force(self):
         for gw in self.units(GATEWAY).ready.noqueue:
-            if self.can_afford(STALKER) and self.supply_left > 0:
+            if self.can_afford(STALKER) and self.supply_left >= 2:
                 await self.do(gw.train(STALKER))
+
+    def find_target(self, state):
+        if len(self.known_enemy_units) > 0:
+            return random.choice(self.known_enemy_units)
+        elif len(self.known_enemy_structures) > 0:
+            return random.choice(self.known_enemy_structures)
+        else:
+            return self.enemy_start_locations[0]
+
+    async def attack(self):
+        if self.units(STALKER).amount > 15:
+            for s in self.units(STALKER).idle:
+                await self.do(s.attack(self.find_target(self.state)))
+
+        elif self.units(STALKER).amount > 4:
+            if len(self.known_enemy_units) > 0:
+                for s in self.units(STALKER).idle:
+                    await self.do(s.attack(random.choice(self.known_enemy_units)))
 
 
 run_game(maps.get("AbyssalReefLE"), [
